a470f4f2b8296335280fb45db86482332e461ae6
myhades <myhades.alaric@gmail.com>
2025-04-15 23:36:34 +0800

    Update IDEA settings and add Maven wrapper

--- se_app/src/main/java/org/group35/Main.java
+++ se_app/src/main/java/org/group35/Main.java
@@ -1,7 +1,25 @@
 package org.group35;
 
-public class Main {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        Label label = new Label("Hello, JavaFX!");
+        StackPane root = new StackPane(label);
+        Scene scene = new Scene(root, 400, 300);
+
+        primaryStage.setTitle("JavaFX Demo");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        launch(args); // 启动 JavaFX 应用
     }
 }
